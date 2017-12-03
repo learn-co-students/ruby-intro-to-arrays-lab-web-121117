70e8e4b7fa26e0ed9138897ccf86a9454a5a4968
github username <github email address>
2017-12-03 13:58:04 -0800

    Done.

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,37 +1,35 @@
 def instantiate_new_array
-    []
+  []
 end
-
-size = instantiate_new_array.size
 
 def array_with_two_elements
-    ["dog", "cat"]
+  ["element_one", "element_two"]
 end
 
-taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of the Woods"]
 
 def first_element(taylor_swift)
-    return taylor_swift[0]
+  taylor_swift[0]
 end
 
 def third_element(taylor_swift)
-    return taylor_swift[2]
+  taylor_swift[2]
 end
 
 def last_element(taylor_swift)
-    return taylor_swift[-1]
+  taylor_swift[-1]
 end
 
 south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
 
 def first_element_with_array_methods(south_east_asia)
-    return south_east_asia[0]
+  south_east_asia[0]
 end
 
 def last_element_with_array_methods(south_east_asia)
-    return south_east_asia[-1]
+  south_east_asia[-1]
 end
 
 def length_of_array(south_east_asia)
-    return south_east_asia.length
+  south_east_asia.length
 end
